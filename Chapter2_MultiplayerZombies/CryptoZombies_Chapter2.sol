4e5dff89d9c1a44ec34582d21a32f0b6bf5ea6f7
Medha08 <medha8pandey@gmail.com>
2019-03-04 21:27:29 +0530

    Add Chapter 2 files

---
+++ Chapter2_MultiplayerZombies/CryptoZombies_Chapter2.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.5.0;
+
+contract ZombieFactory{
+    
+    event NewZombie(uint id , string name, uint dna);
+    
+    uint dnaDigits = 16; // Zombie's DNA
+    uint dnaModulus = 10 ** dnaDigits; // Zombie's DNA  can later use the modulus operator % to shorten an integer to 16 digits
+    
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+    
+    Zombie[] public zombies; // store Zombie structs ie acts as database for state variables
+    
+    mapping(uint => address) public  zombieToOwner; // id of zombie mapped to an zombieToOwner
+    mapping(address => uint) ownerZombieCount; // how many zombie an owner has
+
+    function _createZombie(string memory _name,uint _dna) internal{ // first private only functions within the contract can call this ; internal cause ZombieFeeding calls this functions
+        uint id = zombies.push(Zombie(_name,_dna))-1;
+        
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        
+        emit NewZombie(id,_name,_dna);
+    }
+    
+    function _generateRandomDna(string memory  _str) private view returns(uint){
+        uint rand = uint ( keccak256(abi.encodePacked( _str )));
+        return rand % dnaModulus;
+    }
+    
+    function createRandomZombie(string memory  _name)public{  //function to create a Zombie with random dna
+        require(ownerZombieCount[msg.sender] == 0); // zombie owner can create only one zombie 
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name,randDna);
+        
+    }
+}
+
---
+++ Chapter2_MultiplayerZombies/CryptoZombies_Chapter21.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.5.0;
+
+import "browser/CryptoZombies_Chap2.sol";
+
+ contract KittyInterface{
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract ZombieFeeding is ZombieFactory{
+    
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+    
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+        require(msg.sender ==  zombieToOwner[_zombieId]);
+        
+        Zombie storage myZombie = zombies[_zombieId];
+        
+        _targetDna = _targetDna % dnaModulus;
+         uint newDna = (myZombie.dna + _targetDna) / 2;
+         if(keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))){
+             newDna = newDna - newDna % 100 + 99; //Assume newDna is 334455. Then newDna % 100 is 55, so newDna - newDna % 100 is 334400. Finally add 99 to get 334499.
+         }
+         
+         _createZombie("NoName",newDna);
+         
+          
+        
+    }
+    
+    function feedOnKitty(uint _kittyId,uint _zombieId)public{
+        uint kittyDna;
+        ( , , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        
+        feedAndMultiply(_zombieId,kittyDna,"kitty");
+        
+    }
+/* This is how you do multiple assignment:
+            (a, b, c) = multipleReturns();
+             // We can just leave the other fields blank:
+            (,,c) = multipleReturns();
+            */
+    
+}
