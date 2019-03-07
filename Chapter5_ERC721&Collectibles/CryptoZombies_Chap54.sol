7907fbdd9fa5841a181184b206f3d36946bef9ff
Medha08 <medha8pandey@gmail.com>
2019-03-07 19:45:42 +0530

    Added Chapter 5 Solutions

---
+++ Chapter5_ERC721&Collectibles/CryptoZombies_Chap54.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.0;
+
+import "./CryptoZombies_Chap53.sol";
+import "./erc721.sol";
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
+    
+    mapping (uint => address) zombieApprovals ;
+
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+     return zombieToOwner[_tokenId];
+  }
+  
+  function _transfer(address _from, address _to, uint256 _tokenId) private{
+      ownerZombieCount[_from] = ownerZombieCount[_from].add(1);
+      ownerZombieCount[_to] = ownerZombieCount[_to].sub(1);
+      zombieToOwner[_tokenId] = _to;
+      emit Transfer( _from,  _to,  _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from,_to,_tokenId);
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId){
+        zombieApprovals[_tokenId] = _approved;
+        emit Approval( msg.sender, _approved,_tokenId);
+  }
+}
---
+++ Chapter5_ERC721&Collectibles/CrytpZombies_Chap51.sol
@@ -0,0 +1,73 @@
+pragma solidity ^0.5.0;
+
+import "./CryptoZombies_Chap5.sol";
+contract KittyInterface{
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
+
+contract ZombieFeeding is ZombieFactory{
+    
+   
+    KittyInterface kittyContract;
+    
+     modifier onlyOwnerOf(uint _zombieId){
+        require(zombieToOwner[_zombieId] == msg.sender);
+        _;
+    }
+    
+    function setKittyContractAddress(address _address) external onlyOwner {
+        kittyContract = KittyInterface(_address);
+    }
+    
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal onlyOwnerOf( _zombieId) {
+        
+        Zombie storage myZombie = zombies[_zombieId];
+        require(_isReady(myZombie));
+        
+        _targetDna = _targetDna % dnaModulus;
+         uint newDna = (myZombie.dna + _targetDna) / 2;
+         if(keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))){
+             newDna = newDna - newDna % 100 + 99; //Assume newDna is 334455. Then newDna % 100 is 55, so newDna - newDna % 100 is 334400. Finally add 99 to get 334499.
+         }
+         
+         _createZombie("NoName",newDna);
+         _triggerCooldown(myZombie);
+         
+          
+        
+    }
+    
+    function _triggerCooldown(Zombie storage _zombie) internal  {
+        _zombie.readyTime = uint32(now + coolDownTime);
+    }
+    
+    function _isReady(Zombie storage _zombie) internal view returns(bool){
+        return (_zombie.readyTime <= now);
+         
+    }
+    
+    function feedOnKitty(uint _kittyId,uint _zombieId)public{
+        uint kittyDna;
+        ( , , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        
+        feedAndMultiply(_zombieId,kittyDna,"kitty");
+        
+    }
+/* This is how you do multiple assignment:
+            (a, b, c) = multipleReturns();
+             // We can just leave the other fields blank:
+            (,,c) = multipleReturns();
+            */
+    
+}
