cf8d169d9bf0fa52dc4193aca172ae97f6b099fe
Medha08 <medha8pandey@gmail.com>
2019-03-09 22:53:26 +0530

    Add Chapter 6 files

---
+++ Chapter6_AppFrontend&Web3.js/CryptoZombies_Chap62.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.5.0;
+
+import "./CrytpZombies_Chap51.sol";
+
+contract ZombieHelper is ZombieFeeding{
+    
+    uint levelUpFee = 0.01 ether;
+    
+    modifier aboveLevel(uint _level,uint _zombieId){
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+    
+    function levelUp(uint _zombieId)external payable{
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level = zombies[_zombieId].level.add(1);
+    }
+    
+   function changeName(uint _zombieId , string calldata _newName) external aboveLevel(2,_zombieId)  onlyOwnerOf( _zombieId) {
+        zombies[_zombieId].name = _newName;
+    }
+    
+    function changeDna(uint _zombieId , uint _newDna) external aboveLevel(20,_zombieId)  onlyOwnerOf( _zombieId) {
+        zombies[_zombieId].dna = _newDna;
+    }
+    
+    
+    function withdraw() external onlyOwner {
+        address payable _owner = address(uint160(owner)); // converting non payable addr to payable
+         _owner.transfer(address(this).balance);
+    }
+    function setLevelUpfee(uint _fee) external onlyOwner{
+        levelUpFee = _fee;
+    } 
+    
+  function getZombiesByOwner(address _owner) external view returns(uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for(uint i = 0; i < zombies.length;i++){
+      if(zombieToOwner[i] == _owner)
+        {
+          result[counter] = i;
+          counter++;
+        }
+      }
+    return result;
+  }
+ 
+}
+/*Note: memory arrays must be created with a length argument (in this example, 3). They currently cannot be resized like storage arrays can with array.push(), although this may be changed in a future version of Solidity
+*/
---
+++ Chapter6_AppFrontend&Web3.js/CryptoZombies_Chap64.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.0;
+
+import "./CryptoZombies_Chap53.sol";
+import "./erc721.sol";
+
+///@title A Contract that manages transfering zombie ownership
+///@author Medha Pandey
+///@dev Compliant with OpenZeppelin's implementation of the ERC721 spec draft
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
+    
+    mapping (uint => address) zombieApprovals ;
+
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+     return zombieToOwner[_tokenId];
+  }
+  
+  function _transfer(address _from, address _to, uint256 _tokenId) private{
+      ownerZombieCount[_from] = ownerZombieCount[_from].add(1);
+      ownerZombieCount[_to] = ownerZombieCount[_to].sub(1);
+      zombieToOwner[_tokenId] = _to;
+      emit Transfer( _from,  _to,  _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from,_to,_tokenId);
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId){
+        zombieApprovals[_tokenId] = _approved;
+        emit Approval( msg.sender, _approved,_tokenId);
+  }
+}
